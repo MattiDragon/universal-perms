3d7751b160c1455cd65f9c9a8a8e2ba657920436
Sakura Ryoko <sakura@ryoko.soy>
2023-10-03 10:10:29 -0400

    Cleanup

--- src/main/java/io/github/mattidragon/universalperms/UniversalPerms.java
+++ src/main/java/io/github/mattidragon/universalperms/UniversalPerms.java
@@ -5,7 +5,7 @@ import java.util.Deque;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Predicate;
+// UNUSED: import java.util.function.Predicate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.google.common.collect.Iterables;
