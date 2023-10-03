fad5e850aad49c4967c04e524cc77d1fdde6cbce
Sakura Ryoko <sakura@ryoko.soy>
2023-10-03 09:57:03 -0400

    Update to 1.20.2
    Gradle upgraded to 8.3, Updated Loom, etc.

--- src/main/java/io/github/mattidragon/universalperms/UniversalPerms.java
+++ src/main/java/io/github/mattidragon/universalperms/UniversalPerms.java
@@ -1,5 +1,13 @@
 package io.github.mattidragon.universalperms;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.google.common.collect.Iterables;
 import com.mojang.brigadier.tree.CommandNode;
 import io.github.mattidragon.universalperms.mixin.CommandNodeAccess;
@@ -10,24 +18,23 @@ import net.fabricmc.fabric.api.event.Event;
 import net.fabricmc.fabric.api.util.TriState;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.util.Identifier;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.*;
 
 public class UniversalPerms implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger(UniversalPerms.class);
 
     @Override
     public void onInitialize() {
-        CommandRegistrationCallback.EVENT.addPhaseOrdering(Event.DEFAULT_PHASE, new Identifier("universal_perms", "after"));
-        CommandRegistrationCallback.EVENT.register(new Identifier("universal_perms", "after"), (dispatcher, registryAccess, environment) -> {
-            alterNode(dispatcher.getRoot(), new ArrayDeque<>(), new HashMap<>());
-            LOGGER.info("Applied cursed permissions!");
-        });
+        CommandRegistrationCallback.EVENT.addPhaseOrdering(Event.DEFAULT_PHASE,
+                new Identifier("universal_perms", "after"));
+        CommandRegistrationCallback.EVENT.register(new Identifier("universal_perms", "after"),
+                (dispatcher, registryAccess, environment) -> {
+                    alterNode(dispatcher.getRoot(), new ArrayDeque<>(), new HashMap<>());
+                    LOGGER.info("Applied cursed permissions!");
+                });
     }
 
-    private static void alterNode(CommandNode<ServerCommandSource> node, Deque<String> location, Map<CommandNode<ServerCommandSource>, String> visited) {
+    private static void alterNode(CommandNode<ServerCommandSource> node, Deque<String> location,
+            Map<CommandNode<ServerCommandSource>, String> visited) {
         var name = node.getName();
         if (!name.isEmpty())
             location.addLast(name);
@@ -35,7 +42,8 @@ public class UniversalPerms implements ModInitializer {
         var nodeLocation = String.join(".", location);
 
         if (visited.containsKey(node)) {
-            LOGGER.warn("Encountered same node a two different places, '{}' and '{}'. This shouldn't happen!", visited.get(node), nodeLocation);
+            LOGGER.warn("Encountered same node a two different places, '{}' and '{}'. This shouldn't happen!",
+                    visited.get(node), nodeLocation);
             if (!name.isEmpty())
                 location.removeLast();
             return;
@@ -43,7 +51,10 @@ public class UniversalPerms implements ModInitializer {
 
         var permission = createPermission("use", location);
         var requirement = node.getRequirement();
-        ((CommandNodeAccess)node).setRequirement((ServerCommandSource source) -> (source.getEntity() == null ? TriState.DEFAULT : Permissions.getPermissionValue(source, permission)).orElseGet(() -> requirement.test(source)));
+        ((CommandNodeAccess) node)
+                .setRequirement((ServerCommandSource source) -> (source.getEntity() == null ? TriState.DEFAULT
+                        : Permissions.getPermissionValue(source, permission))
+                        .orElseGet(() -> requirement.test(source)));
 
         node.getChildren().forEach(child -> alterNode(child, location, visited));
 
